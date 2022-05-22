78083668c5145805bfe397d2b8a063666dbe09a9
CATALINA CIFUENTES <c.cifuentes@uc.cl>
2022-05-22 15:14:32 -0400

    arreglo conexion

--- Sites/conexion.php
+++ Sites/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conexion = pg_connect("host=localhost dbname=grupo75e2 user = grupo75 password=iñakiycata" );
+    $conexion = pg_connect("host=codd.ing.puc.cl dbname=grupo75e2 user=grupo75 password=iñakiycata");
 
 } catch (Exception $error){
     echo "No se pudo conectar a la base de datos";
