6ba8f61c18cd12b1a7ad7a29bbb698dab7f20cf3
CATALINA CIFUENTES <c.cifuentes@uc.cl>
2022-05-22 15:46:58 -0400

    conexion

--- Sites/conexion.php
+++ Sites/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conexion = new PDO("pgsql:dbname=grupo75e2;host=codd.ing.puc.cl;port=5432;user=grupo75;password=iñakiycata");
+    $conexion = new PDO("pgsql:dbname=grupo75e2;host=codd.ing.puc.cl;port=2552;user=grupo75;password=iñakiycata");
 } catch (Exception $error){
     echo "No se pudo conectar a la base de datos: $error";
 }
