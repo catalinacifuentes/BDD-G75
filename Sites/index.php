de9cf383be10112c48d9e0ef0ed76313d1bfac58
CATALINA CIFUENTES <c.cifuentes@uc.cl>
2022-05-23 00:55:02 -0400

    primer intento dropdown

--- Sites/index.php
+++ Sites/index.php
@@ -12,6 +12,29 @@
                     <input type="submit" name="Buscar">
 
     </form>
+                <li>Dado un código ICAO de un aeródromo ingresado por el usuario y una aerolínea seleccionada por el usuario, liste todos los vuelos aceptados de dicha aerolínea que tienen como destino el aeródromo.</li>
+                <form align="center" action="consulta_2.php" method="post">
+                    <label> Nombre Aerolinea<label>
+                    <select name="aerolinea">
+                    <option value="0">Seleccione</option>
+                    <?php
+                    include('conexion.php');
+                    $aerolinea="SELECT DISTINCT nombre_compania
+                                FROM compania_aerea;";
+                    $result = $conexion -> prepare($query);
+                    $result -> execute();
+                    $data = $result -> fetchAll();
+                    foreach ($data as $d){
+                        echo '<option_value="'.$d[0].'">'.$d[0].'</option>';
+                    }           
+
+                    ?>
+                    </select><br><br>
+                    <p> Ingrese codigo ICAO </p>
+                    <input type="text" name="codigo">
+                    <p> Haz click para ver la consulta </p>
+                    <input type="submit" name="Buscar">
+    </form>
                 <li>Dado un código de reserva ingresado por el usuario, liste los tickets asociados a esta junto a sus pasajeros y costos.</li>
                 <form align="center" action="consulta_3.php" method="post">
                     <p> Ingrese codigo de reserva </p>
