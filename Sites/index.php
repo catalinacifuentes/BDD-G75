4aa205ac73cc273767d17f2c09820bf0486f9efa
CATALINA CIFUENTES <c.cifuentes@uc.cl>
2022-05-23 00:57:28 -0400

    mas ordenado

--- Sites/index.php
+++ Sites/index.php
@@ -11,7 +11,8 @@
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
 
-    </form>
+                </form>
+
                 <li>Dado un código ICAO de un aeródromo ingresado por el usuario y una aerolínea seleccionada por el usuario, liste todos los vuelos aceptados de dicha aerolínea que tienen como destino el aeródromo.</li>
                 <form align="center" action="consulta_2.php" method="post">
                     <label> Nombre Aerolinea<label>
@@ -34,7 +35,8 @@
                     <input type="text" name="codigo">
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
-    </form>
+                </form>
+
                 <li>Dado un código de reserva ingresado por el usuario, liste los tickets asociados a esta junto a sus pasajeros y costos.</li>
                 <form align="center" action="consulta_3.php" method="post">
                     <p> Ingrese codigo de reserva </p>
@@ -42,25 +44,28 @@
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
 
-    </form>                     
+                </form>        
+
                 <li>Por cada aerolínea, muestre al cliente que ha comprado la mayor cantidad de tickets.</li>
                 <form align="center" action="consulta_4.php" method="post">
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
 
-    </form>
+                </form>
+
                 <li>Al ingresar el nombre de una aerolínea, liste la cantidad de vuelos que tienen en cada uno de los estados.</li>
                 <form align="center" action="consulta_5.php" method="post">
                     <p> Ingrese nombre de la aerolinea </p>
                     <input type="text" name="aerolinea">
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
-    </form>
+                </form>
+
                 <li>Muestre la aerolínea que tiene el mayor porcentaje de vuelos aceptados.</li>
                 <form align="center" action="consulta_6.php" method="post">
                     <p> Haz click para ver la consulta </p>
                     <input type="submit" name="Buscar">
-    </form>
+                </form>
             </ul>
     </body>
 <footer>
