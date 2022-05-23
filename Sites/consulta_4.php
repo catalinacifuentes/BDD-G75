c78d7effda08c839aa8a36b4578eba8539826d5c
CATALINA CIFUENTES <c.cifuentes@uc.cl>
2022-05-23 01:46:48 -0400

    cambio consulta4

--- Sites/consulta_4.php
+++ Sites/consulta_4.php
@@ -1,12 +1,12 @@
 <body>
 <?php
         require('conexion.php');
-        $query = "SELECT  MIN(tabla.nombre_compania), tabla.pasaporte_comprador, MAX(tabla.maximo_ticket) as maximo_ticket
-        FROM ( SELECT reserva.pasaporte_comprador, compania_aerea.nombre_compania, COUNT(reserva.numero_ticket) as maximo_ticket
+        $query = "SELECT MIN(tabla.pasaporte_comprador), tabla.nombre_compania, MAX(tabla.maximo_ticket) as maximo_ticket
+        FROM (SELECT reserva.pasaporte_comprador, compania_aerea.nombre_compania, COUNT(reserva.numero_ticket) as maximo_ticket
         FROM reserva, ticket, vuelo, compania_aerea
         WHERE reserva.numero_ticket = ticket.numero_ticket AND vuelo.vuelo_id = ticket.vuelo_id AND compania_aerea.codigo_compania = vuelo.codigo_compania
         GROUP BY reserva.pasaporte_comprador,  compania_aerea.nombre_compania)as tabla
-        GROUP BY tabla.nombre_compania";
+        GROUP BY tabla.nombre_compania;";
         $result = $conexion -> prepare($query);
         $result -> execute();
 
